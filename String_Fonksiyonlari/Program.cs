75b0b7649a58f9ba9aeaa4960296752199eea45d
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:35:25 +0300

    Proje dosyası ekle.

---
+++ String_Fonksiyonlari/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace String_Fonksiyonlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ifade = " Afacan";
+
+            Console.WriteLine(ifade);
+            Console.WriteLine(ifade.Length);
+            Console.WriteLine(ifade.Trim());
+            Console.WriteLine(ifade.Trim().Length);
+            Console.WriteLine(ifade.TrimStart());
+            Console.WriteLine(ifade.TrimEnd());
+            Console.WriteLine(ifade.ToLower());
+            Console.WriteLine(ifade.ToUpper());
+            Console.WriteLine(ifade.Replace('a', 'e'));
+        }
+    }
+}
